afb831d93e61fe012e155150aad5d1dc32077152
guzus <storminggalaxys3@gmail.com>
2022-07-24 11:57:16 +0900

    fetch urls from server & deploy config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+
+function Image(props) {
+  return (
+    <img src={props.url} alt="crypto-meme">
+      {props.value}
+    </img>
+  );
+}
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://crypto-meme-server.herokuapp.com/image')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
+        );
+      }
+      return response.json();
+    })
+    .then((actualData) => {
+      setData(actualData);
+      setError(null);
+    })
+    .catch((err) => {
+      setError(err.message);
+      setData(null);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {loading && <div>A moment please...</div>}
+        {error && (
+          <div>{`There is a problem fetching the data - ${error}`}</div>
+        )}
+        <ul>
+          {data &&
+            data.map(({ id, url }) => (
+              <div key={id}>
+                {Image({url})}
+              </div>
+            ))}
+        </ul>
     </div>
   );
 }
