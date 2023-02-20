37a636a8c77abe7e55c353c2e61b821a2ec33a37
guzus <storminggalaxys3@gmail.com>
2023-02-21 01:42:59 +0900

    accessible from external by link

--- src/index.js
+++ src/index.js
@@ -2,22 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import ReactGA from 'react-ga4'
 const TRACKING_ID = 'G-76SSRFV483' // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+window.addEventListener('load', () => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  )
+})
