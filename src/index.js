c60499830b4945118a972bc00a4890aec5b3b985
guzus <storminggalaxys3@gmail.com>
2023-07-19 16:20:06 +0900

    revert to hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ReactGA from 'react-ga4'
 const TRACKING_ID = 'G-76SSRFV483' // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID)
@@ -10,10 +10,8 @@ ReactGA.initialize(TRACKING_ID)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 window.addEventListener('load', () => {
   root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
   )
 })
