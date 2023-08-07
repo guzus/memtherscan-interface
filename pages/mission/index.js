8d2ab2bed3cc6fd18fa82566666ec45b0a56a58f
guzus <50664161+guzus@users.noreply.github.com>
2023-08-08 02:26:49 +0900

    fix import (#17)

--- pages/mission/index.js
+++ pages/mission/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../Components/Header";
+import Header from "../../components/header";
 import styled from "styled-components";
 
 const TextWrapper = styled.section`
