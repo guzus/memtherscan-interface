a34530726a782dcbbbd1194f25e0bf441238cdc6
guzus <storminggalaxys3@gmail.com>
2023-08-08 01:15:52 +0900

    move react js to next js

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Memtherscan
+
+**The next-gen crypto meme aggregator**
+
+> Crypto memes in the palm of your hand
+
+<img src="https://github.com/guzus/memtherscan/assets/50664161/535a0983-09ee-467b-bbd3-4db1f9173a65" width=400 />
+
+## Development
+
+Run on localhost
+
+```
+npm run dev
+```
+
+Deploy (using Vercel)
+
+```
+npm run deploy
+```
+
+## Infrastructure
+
+- **Cloudflare R2**: Image, Video Storage
+- **Firebase**: Database
+- **GCP Cloud Run**: [API Server](https://github.com/guzus/memtherscan-server) (not open-source yet)
+- **Github Actions, Github Pages**: [Frontend](https://github.com/guzus/memtherscan) Deployment
--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,142 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useSearchParams, usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import React, { useEffect, useState, useCallback } from "react";
+import Image from "../components/image/index.js";
+import Header from "../components/header";
+import { BASE_URL } from "../constants";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import "react-horizontal-scrolling-menu/dist/styles.css";
 
-export default function Home() {
+function TagBar(createQueryString) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const tags = [
+    { title: "latest", key: "sortByTimestamp", value: "desc" },
+    { title: "trending", key: "tag", value: "trending" },
+    { title: "ethereum", key: "tag", value: "ethereum" },
+  ];
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {tags.map(({ title, key, value }) => (
+        <button
+          onClick={() => {
+            router.push(pathname + "?" + createQueryString(key, value));
+          }}
+          // itemId={searchParam} // NOTE: itemId is required for track items
+          // title={title}
+          // key={searchParam}
+          // selected={isItemSelected(id)}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+          {title}
+        </button>
+      ))}
+      {/*<TagCard title={"upload"} key={"upload"} />*/}
+      <Link href="/upload"> upload </Link>
+      <Link href="/mission"> about us </Link>
+    </ScrollMenu>
+  );
 }
+
+function TagCard({ onClick, selected, title, itemId }) {
+  const visibility = React.useContext(VisibilityContext);
+
+  return (
+    <button onClick={() => onClick(visibility)} tabIndex={0} className="tag">
+      <div>{title}</div>
+      {/* <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+        <div>selected: {JSON.stringify(!!selected)}</div> */}
+    </button>
+  );
+}
+
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } =
+    React.useContext(VisibilityContext);
+
+  return (
+    <div disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+      {/* Left */}
+    </div>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+
+  return (
+    <div disabled={isLastItemVisible} onClick={() => scrollNext()}>
+      {/* Right */}
+    </div>
+  );
+}
+
+function Home() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const searchParams = useSearchParams();
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  useEffect(() => {
+    const tag = searchParams.get("tag");
+    const sortByTimestamp = searchParams.get("sortByTimestamp");
+    let url;
+    if (tag) {
+      url = BASE_URL + `/image?shuffle=true&tag=${tag}`;
+    } else {
+      url = BASE_URL + `/image?shuffle=true`;
+    }
+    if (sortByTimestamp) {
+      url += `&sortByTimestamp=${sortByTimestamp}`;
+    }
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`,
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [searchParams]);
+
+  return (
+    <div className="App">
+      <Header />
+      {TagBar(createQueryString)}
+      {loading && <div>A moment please...</div>}
+      {error && <div>{`There is a problem fetching the data - ${error}`}</div>}
+      <div>
+        {data &&
+          data.map(({ id, url }) => <div key={id}>{Image({ id, url })}</div>)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
